368a0ca99c4f76320912f63999290a11df556454
Burke Azbill <burkeazbill@gmail.com>
2019-08-13 18:31:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Get Octant
+
+This repo holds a script that may be used to retrieve the current version of VMware's [Octant](https://github.com/vmware/octant) developer dashboard.
+
+The script is intended for use with MacOS and Linux operating systems. It uses the output of uname -s to determine the current OS. It also checks the Octant releases page for the current version, then uses that version info plus the OS info to construct the correct url to download the file from.
+
+By default, the script will download the .tar.gz file. If you are running Linux and prefer to get the deb or rpm file, simply pass one of those two extensions as a parameter to the script:
+
+```bash
+./getOctant.sh deb
+
+
+./getOctant.sh rpm
+```
---
+++ getOctant.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Author: Burke Azbill
+# Script Name: getOctant.sh
+# Purpose: Retrieve the current version of Octant for MacOS/Linux environments
+# Date: Aug 13, 2019
+# Set Variables:
+OS="`uname -s`"
+FILE_EXT="tar.gz"
+
+# Detect the platform and reset OS for URL generation
+case $OS in
+  'Linux')  OS='Linux'  ;;
+  'Darwin') OS='macOS'    ;;
+  *) ;;
+esac
+
+# Check for Command Line Parameters:
+if [ ${#} -gt 0 ]; then
+  if [ "$OS" == "Linux" ]; then
+    case $1 in
+      '?') echo "Usage: getOctant.sh ext"; echo "  getOctant.sh rpm"; echo "  Valid ext: rpm,deb,gz - if not specified, gz is default";;
+      'deb')  FILE_EXT="deb";;
+      'rpm')  FILE_EXT="rpm";;
+      'gz')   FILE_EXT="tar.gz";;
+      *)      FILE_EXT="tar.gz";;
+    esac
+  fi
+fi
+
+# The following wget command retrieves the latest version number for Octant
+export OCTANT_VERSION=$(wget -qO- https://github.com/vmware/octant/releases/latest | sed -nE 's|.*"Recent Commits to octant:v(.*)\" .*|\1|p')
+
+# Build the filename to download:
+FILENAME="octant_$OCTANT_VERSION""_$OS-64bit"
+echo https://github.com/vmware/octant/releases/download/v$OCTANT_VERSION/$FILENAME.$FILE_EXT
+# Now retrieve the file:
+curl -L https://github.com/vmware/octant/releases/download/v$OCTANT_VERSION/$FILENAME.$FILE_EXT -o $FILENAME.$FILE_EXT 
+ls -ahl $FILENAME.$FILE_EXT
+if [ "$FILE_EXT" == "tar.gz" ]; then
+  tar -zxvf $FILENAME.$FILE_EXT
+  cd $FILENAME
+  ./octant version
+fi
